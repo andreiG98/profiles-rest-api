69966cb75fe6ab68374c96faa6b30cc77598bf9d
Andrei Gidea <andrei-cristian.gidea@my.fmi.unibuc.ro>
2022-02-04 15:02:29 +0200

    greseala folder

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/andreiG98/profiles-rest-api.git'
 
-PROJECT_BASE_PATH='/home/andreig/Courses/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
 apt-get update
